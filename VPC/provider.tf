f4c1a26ba3f6bc232e1e098c0f1b5e6683607bbd
mackdatadrive2 <mackdatadrive2@gmail.com>
2025-07-22 10:20:01 +0000

    updated folders

--- VPC/provider.tf
+++ VPC/provider.tf
@@ -2,5 +2,5 @@
 
 provider "aws" {
   region                  = "us-east-1"
-  shared_credentials_file = "<Your AWS Credentials File path>"
+ # shared_credentials_file = "<Your AWS Credentials File path>"
 }
--- VPC/vars.tf
+++ VPC/vars.tf
@@ -1,6 +1,3 @@
-# Author :- Dheeraj Choudhary
-
-variable "AWS_REGION" 
-{ 
+variable "aws_region" { 
 default = "us-east-1" 
 }
